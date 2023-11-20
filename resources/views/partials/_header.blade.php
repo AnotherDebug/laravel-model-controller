ff1d215b1dff3aabd0af26ec5006235974011e6e
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-20 17:43:15 +0100

    [ADD]: class active

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -3,10 +3,10 @@
     <div class="container">
         <ul class="nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+              <a class="nav-link {{Route::currentRouteName() == "home" ? "active" : "" }}" aria-current="page" href="{{route('home')}}">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="{{route('movies')}}">Movies</a>
+              <a class="nav-link {{Route::currentRouteName() == "movies" ? "active" : "" }}" href="{{route('movies')}}">Movies</a>
             </li>
           </ul>
     </div>
