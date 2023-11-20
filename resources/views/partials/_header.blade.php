e66f3253c3ad930458ce0b54777cbf91a13ad1f0
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-20 17:37:35 +0100

    [ADD]: route href link

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -6,10 +6,7 @@
               <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link" href="{{route('movies')}}">Movies</a>
             </li>
           </ul>
     </div>
