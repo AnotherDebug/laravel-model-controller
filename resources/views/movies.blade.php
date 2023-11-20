0257bd709467c831f1a954373d78f7670ce25a05
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-20 18:04:38 +0100

    [ADD]: card e foreach loop movies

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.main')
 
 @section('content')
+    <h1>Movies List</h1>
 
-<h1>Movies List</h1>
+    <div class="container d-flex flex-wrap">
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem suscipit autem iure voluptates repellendus harum ea fugiat omnis nam ipsa delectus tempore, molestiae reiciendis architecto temporibus pariatur! Veniam, suscipit eius corporis asperiores inventore dolorem qui ad! Numquam, vel. Itaque voluptatum maxime et adipisci! Qui dolorem fugit nemo dicta dignissimos eveniet ratione, nam eos inventore voluptatibus placeat harum ut tempore consequuntur veritatis! Reprehenderit dolore architecto aperiam nesciunt tempore repellat delectus ad rerum aspernatur excepturi nobis, voluptatibus mollitia odio libero? Laboriosam voluptate animi, odio voluptas corrupti aliquam deserunt! Quam at delectus neque nihil reprehenderit exercitationem, incidunt molestias itaque nam, quisquam ea odit accusamus doloremque. Omnis rem cumque voluptates nostrum, voluptatum nam dolores ullam sit, tempora libero non natus animi eos eaque inventore! Minima reiciendis quam quo odit amet impedit, error, earum nihil vitae nesciunt consequatur sequi fugiat, obcaecati veniam in porro debitis facere dolorum? Modi tempore amet eum consequuntur optio, ipsum doloremque cumque commodi autem in incidunt perferendis! Debitis itaque quidem distinctio modi recusandae commodi aliquid voluptas aperiam assumenda sed veritatis, ipsam pariatur vel incidunt harum voluptatibus alias repudiandae quaerat exercitationem est rerum deleniti perferendis maiores tenetur! Dolore voluptates aliquid id facere. Reprehenderit, quis officiis fugiat commodi reiciendis quasi consectetur aliquid nesciunt.</p>
+        @foreach ($movies as $movie)
+            {{-- SINGOLA CARD --}}
+            <div class="card mx-3 my-3 m-auto" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie->title}}</h5>
+                    <a href="#" class="btn btn-primary">Vai</a>
+                </div>
+            </div>
+        @endforeach
 
+
+
+    </div>
 @endsection
 
 
