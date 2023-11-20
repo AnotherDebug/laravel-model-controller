4196ef4e76c906498fddb6c94ed600bbc50c5d78
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-20 17:36:20 +0100

    [ADD]: movies php

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+
+<h1>Movies List</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem suscipit autem iure voluptates repellendus harum ea fugiat omnis nam ipsa delectus tempore, molestiae reiciendis architecto temporibus pariatur! Veniam, suscipit eius corporis asperiores inventore dolorem qui ad! Numquam, vel. Itaque voluptatum maxime et adipisci! Qui dolorem fugit nemo dicta dignissimos eveniet ratione, nam eos inventore voluptatibus placeat harum ut tempore consequuntur veritatis! Reprehenderit dolore architecto aperiam nesciunt tempore repellat delectus ad rerum aspernatur excepturi nobis, voluptatibus mollitia odio libero? Laboriosam voluptate animi, odio voluptas corrupti aliquam deserunt! Quam at delectus neque nihil reprehenderit exercitationem, incidunt molestias itaque nam, quisquam ea odit accusamus doloremque. Omnis rem cumque voluptates nostrum, voluptatum nam dolores ullam sit, tempora libero non natus animi eos eaque inventore! Minima reiciendis quam quo odit amet impedit, error, earum nihil vitae nesciunt consequatur sequi fugiat, obcaecati veniam in porro debitis facere dolorum? Modi tempore amet eum consequuntur optio, ipsum doloremque cumque commodi autem in incidunt perferendis! Debitis itaque quidem distinctio modi recusandae commodi aliquid voluptas aperiam assumenda sed veritatis, ipsam pariatur vel incidunt harum voluptatibus alias repudiandae quaerat exercitationem est rerum deleniti perferendis maiores tenetur! Dolore voluptates aliquid id facere. Reprehenderit, quis officiis fugiat commodi reiciendis quasi consectetur aliquid nesciunt.</p>
+
+@endsection
+
+
+@section('title')
+    | Movies
+@endsection
