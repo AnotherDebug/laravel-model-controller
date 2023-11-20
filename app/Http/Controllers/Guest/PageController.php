89f9f0dc674711412fe59ff476f07d4e8ed753cf
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-20 17:36:47 +0100

    [ADD]: movies function

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,4 +10,8 @@ class PageController extends Controller
     public function index() {
         return view('home');
     }
+
+    public function movies() {
+        return view('movies');
+    }
 }
